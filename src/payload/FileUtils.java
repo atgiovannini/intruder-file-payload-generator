80ff98498e691b8f5216cb601121fe901c1af411
agiovannini <aaron.giovannini@byteheed.com>
2019-04-29 10:18:05 +0200

    simple ignore patter by starting with... pending to improve

--- src/payload/FileUtils.java
+++ src/payload/FileUtils.java
@@ -9,16 +9,38 @@ import java.util.ArrayList;
  */
 public class FileUtils {
         
+    static String[] excludedFiles = new String[]{{"readme", ".", "licen"}};
+    static String[] excludedFolders = new String[]{".git", ".vscode", ".settings"};
     public static ArrayList<String> listFilesForFolder(String folder) {
         ArrayList<String> files = new ArrayList<>();
                
         File inputFolder = new File(folder);
         for (final File fileEntry : inputFolder.listFiles()) {
+            String name = fileEntry.getName();
+            boolean con = false;
+
             if (fileEntry.isDirectory()) {
+                // Needs to improve ignore patterns
+                for (String exc : FileUtils.excludedFolders) {
+                    if (name.toLowerCase().startsWith(exc)) {
+                        con = true;
+                        break;
+                    }
+                }
+                if (con) continue;
                 files.addAll(listFilesForFolder(fileEntry.getAbsolutePath()));
             } else {
+                // Needs to improve ignore patterns
+                for (String exc : FileUtils.excludedFiles) {
+                    if (name.toLowerCase().startsWith(exc)) {
+                        con = true;
+                        break;
+                    }
+                }
+                if (con) continue;
                 files.add(fileEntry.getAbsolutePath());
             }
+
         }
         
         return files;
